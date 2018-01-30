e537d55c3c5e8dc5a8cb0f6121f4e233fd051041
Shikhar Dubey <shikhar.db@gmail.com>
2018-01-30 16:27:44 +0530

    Basic skeleton of MEAN app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));

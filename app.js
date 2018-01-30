9cb8b65a2e05b8f1807041039e9d707b517d383d
Shikhar Dubey <shikhar.db@gmail.com>
2018-01-30 23:10:44 +0530

    Device data now returning

--- app.js
+++ app.js
@@ -46,11 +46,17 @@ function saveTemp(data) {
     })
 }
 
-
+// get home page
 app.get('/', (req, res) => res.render('index'));
 
-app.get('/thermo', (req, res) => {
-
+// get device data [Params : id = device id here, timefrom = time from when the user wants to see]
+app.get('/device/:id', (req, res) => {
+    if(req.params.id) {
+        Temperature.find({ deviceSerialNumber : capitalizeFirstLetter(req.params.id)}).then(function(data) {
+            res.json(data);
+        });
+        
+    }
 });
 
 app.post('/', function (req, res) {
@@ -62,24 +68,28 @@ app.post('/', function (req, res) {
     }
 });
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
 // start cron job
-new CronJob('0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', function () {
-    let dummyDevices = ['Thermometer', 'Battery', 'Radiator'];
-    let randomTemp = Math.ceil(Math.random()*100);
-    let randomDevice = Math.floor(Math.random()*3);
+// new CronJob('0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', function () {
+//     let dummyDevices = ['Thermometer', 'Battery', 'Radiator'];
+//     let randomTemp = Math.ceil(Math.random()*100);
+//     let randomDevice = Math.floor(Math.random()*3);
     
-    let randomDt = randomDate(new Date(2017, 0, 1), new Date());
+//     let randomDt = randomDate(new Date(2017, 0, 1), new Date());
 
-    let mumble = { "device" :  dummyDevices[randomDevice], "temperature" : randomTemp, "timestamp" : randomDt}
+//     let mumble = { "device" :  dummyDevices[randomDevice], "temperature" : randomTemp, "timestamp" : randomDt}
 
-    // save a new data every 5 seconds
-    saveTemp(mumble);
+//     // save a new data every 5 seconds
+//     saveTemp(mumble);
 
- }, null, true, 'America/Los_Angeles');
+//  }, null, true, 'America/Los_Angeles');
 
 
 app.listen(3000, () => console.log('App listening on port 3000!'));

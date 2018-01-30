3d5f3a521d3ad00f1029716fffc2e113a76ee16b
Shikhar Dubey <shikhar.db@gmail.com>
2018-01-30 20:04:20 +0530

    Functioning backend with dummy data and POST request to add temperature

--- app.js
+++ app.js
@@ -1,37 +1,75 @@
+// Require modules
 const express = require('express');
-const app = express();
 const path = require('path');
 const CronJob = require('cron').CronJob;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const app = express();
+
+//use middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // connect to the database
 mongoose.connect('mongodb://localhost/thermo');
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     console.log('we are connected!');
 });
 
 // create temperature schema
-var tempSchema = mongoose.Schema({
+const tempSchema = mongoose.Schema({
     timestamp: { type: Date, default: Date.now },
-    deviceSerialNumber  : String,
-    temperature  : Number
+    deviceSerialNumber: String,
+    temperature: Number
 });
 
 // create mongoose model object
 var Temperature = mongoose.model('Temperature', tempSchema);
 
+function saveTemp(data) {
 
-app.use(express.static(path.join(__dirname, 'public')));
+    var tempdata = new Temperature({
+        deviceSerialNumber: data.device,
+        temperature: data.temperature
+    })
+
+    tempdata.save(function(err, res) {
+        if(err) { return false; }
+        else { return true; }
+    })
+}
 
 
 app.get('/', (req, res) => res.render('index'));
 
+app.post('/', function (req, res) {
+    let result = saveTemp(req.body);
+    if(result === true) {
+        res.status(200).send('Cool');
+    } else {
+        res.status(500).send('Oops');
+    }
+});
+
 // start cron job
-// new CronJob('0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', function () {
-//     console.log('You will see this message every 5 seconds');
-// }, null, true, 'America/Los_Angeles');
+new CronJob('0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', function () {
+    let dummyDevices = ['Thermometer', 'Battery', 'Radiator'];
+    let randomTemp = Math.ceil(Math.random()*100);
+    let randomDevice = Math.floor(Math.random()*3);
+
+    let mumble = { "device" :  dummyDevices[randomDevice], "temperature" : randomTemp}
+
+    // save a new data every 5 seconds
+    saveTemp(mumble);
+
+ }, null, true, 'America/Los_Angeles');
+
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
